1f22087ef6f4d2ee52b37a019b13e641a5edf535
Ivan <lardius@icloud.com>
2021-08-16 15:28:44 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const element = <h1>Hello World</h1>;
+const element = <h1>Hello World меня зовут: *Котов Иван*</h1>;
 
 ReactDom.render(element, document.getElementById("root"));
